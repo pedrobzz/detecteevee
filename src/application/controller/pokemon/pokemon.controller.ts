b7f5a29089ee33d37334521fd543bc27663502f3
pedro bzz <pedrobzz66@gmail.com>
2022-07-15 13:23:29 -0300

    feat(pokemon.controller): Added the useCase GetRandomPokemon to the controller

--- src/application/controller/pokemon/pokemon.controller.ts
+++ src/application/controller/pokemon/pokemon.controller.ts
@@ -1,13 +1,14 @@
 import * as trpc from "@trpc/server";
 import { z } from "zod";
+import { makeGetRandomPokemon } from "../../factories";
 
 export const pokemonController = trpc.router().query("getRandomPokemon", {
-  input: z.number().default(10),
-  resolve({ input }) {
-    const MAX_INDEX = input;
-    const randomIndex = Math.floor(Math.random() * MAX_INDEX);
-    return {
-      randomIndex,
-    };
+  input: z.object({
+    MAX_INDEX: z.number().default(10),
+    totalPokemons: z.number().default(1),
+  }),
+  async resolve({ input }) {
+    const getRandomPokemon = makeGetRandomPokemon();
+    return await getRandomPokemon.execute(input);
   },
 });
