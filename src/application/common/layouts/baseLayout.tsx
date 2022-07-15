ff4e84ae767532ad50a67e3048240a5245f36eb5
pedro bzz <pedrobzz66@gmail.com>
2022-07-14 21:57:05 -0300

    feat(BaseLayout): Added base layout

---
+++ src/application/common/layouts/baseLayout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export const BaseLayout: React.FC = ({ children }): JSX.Element => {
+  return (
+    <div className="bg-zinc-900 w-screen h-screen flex flex-col">
+      <Navbar />
+      <div className="w-full h-full p-12">{children}</div>
+    </div>
+  );
+};
