c8c2de42e607562672718ce94ec1a57bf09cbd61
pedro bzz <pedrobzz66@gmail.com>
2022-07-15 13:23:44 -0300

    feat(nameGuesses): Added useQuery to fetch random pokemons

--- src/pages/nameGuesser.tsx
+++ src/pages/nameGuesser.tsx
@@ -3,11 +3,26 @@
 import React from "react";
 
 /* import { Container } from "./styles"; */
+import { useTRCP } from "../application/common/hooks/useTRCP";
 
 const NameGuesser: React.FC = (): JSX.Element => {
+  // const [name, setName] = useState("");
+  // const ctx = useContext(AppContext);
+  const trpc = useTRCP();
+  const pokemons = trpc.useQuery([
+    "pokemon.getRandomPokemon",
+    { MAX_INDEX: 10, totalPokemons: 5 },
+  ]);
   return (
     <div>
       <h1> Hello, NameGuesser!</h1>
+      <button
+        onClick={() => {
+          console.log(pokemons.data);
+        }}
+      >
+        Log Pokemons
+      </button>
     </div>
   );
 };
